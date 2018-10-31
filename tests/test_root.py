735d63e687a5705552bb94ef1e91535f36c47360
Mathieu Rodic <mathieu@iko-system.com>
2018-10-31 18:24:56 +0100

    [DEBUG] corrected test for root resource

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,4 +1,11 @@
+import common.config
+
+
 class TestRoot(object):
 
     def test_name(self, client):
-        
+        result = client.get('/')
+        assert result.status_code == 200
+        assert isinstance(result.json, dict)
+        assert result.json['service_name'] == common.config.get('rest', 'service_name')
+        assert result.json['service_version'] == common.config.get('rest', 'service_version')
